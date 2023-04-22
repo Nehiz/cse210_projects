83f24185e3c448ceedd95fd23430f54eeb8def34
Nehiz <nehis4all@yahoo.com>
2023-04-22 22:21:10 +0100

    Prep 1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,17 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {   
+        Console.WriteLine();
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+        
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}. ");
+        Console.WriteLine();
+
     }
 }
