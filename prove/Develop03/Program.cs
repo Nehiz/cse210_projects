676f527da64828306221d3903972ef8b96f5cbc4
Nehiz <nehis4all@yahoo.com>
2023-07-23 20:43:10 +0100

    scripture update

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,18 +5,26 @@ class Program
     static void Main()
     {
         Console.WriteLine("Enter the scriture reference (e.g, John 3:16):");
-        string refrence = Console.ReadLine();
+        string reference = Console.ReadLine();
 
         Console.WriteLine("Enter the scripture text:");
         string text = Console.ReadLine();
 
-        Scripture scripture = new Scripture(refrence, text);
+        Scripture scripture = new Scripture(reference, text);
 
-        while (true)
+        Console.Clear();
+        scripture.Display();
+
+        while (!scripture.AllWordsHidden())
         {
-            Console.Clear();
-            Console.WriteLine(scripture.FullReference);
+
+            // Hide one random word at a time
             scripture.HideRandomWord();
+
+            // Display the updated scripture text with one word hidden
+            Console.Clear();
+            scripture.Display();
+
             Console.WriteLine("Press Enter to continue or type 'quit' to end.");
             string input = Console.ReadLine();
             if (input.ToLower() == "quit")
